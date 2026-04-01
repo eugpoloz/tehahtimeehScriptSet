f086765a13635f24fb87db95fce8c12254014703
Evgenia Polozova <eugpoloz@gmail.com>
2026-04-01 23:25:41 +0300

    feat(addProfileLinkToPosts): create post search link

---
+++ src/various/addProfileLinkToPosts.js
@@ -0,0 +1,17 @@
+import { viewprofile } from "../utils";
+
+function addProfileLinkToPosts(forums = [14,15,16,17,5]) {
+  const profileName = document.getElementById('profile-name');
+
+  if (viewprofile && profileName) {
+    const forumsStr = forums.join(',');
+
+    const html = `<a href="/search.php?action=search&keywords=&author=${profileName.textContent}&forums=${forumsStr}&search_in=posts&sort_by=0&sort_dir=DESC&show_as=posts">Показать все посты</a> | `;
+
+    const topicsLink = document.getElementById('user-topics');
+
+    topicsLink.insertAdjacentHTML('beforebegin', html);
+  }
+}
+
+export default addProfileLinkToPosts;
