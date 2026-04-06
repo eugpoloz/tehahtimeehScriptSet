32d95e410cba8e27769fd6cf71f7ed97b8342ce2
Evgenia Polozova <eugpoloz@gmail.com>
2026-04-06 18:42:52 +0300

    fix(utils/handleLogin): update w/ proper redirect

--- src/utils/login.js
+++ src/utils/login.js
@@ -3,30 +3,35 @@ export const handleLogin = async ({ login, password } = {}) => {
     return;
   }
 
+  const redirectUrl = window.location.href;
+
   try {
-  const formData = new FormData();
-  formData.append("form_sent", "1");
-  formData.append("req_username", login);
-  formData.append("req_password", password);
-  formData.append("login", "Submit");
+    const formData = new FormData();
+    formData.append("form_sent", "1");
+    formData.append("req_username", login);
+    formData.append("req_password", password);
+    formData.append("redirect_url", redirectUrl);
+    formData.append("login", "Submit");
 
-  const response = await fetch(`${window.location.origin}/login.php?action=in`, {
-    body: formData,
-    credentials: "include",
-    headers: {
-      "Cache-Control": "max-age=0",
-      "Upgrade-Insecure-Requests": "1"
-    },
-    method: "POST"
-  });
+    const response = await fetch(
+      `${window.location.origin}/login.php?action=in`,
+      {
+        body: formData,
+        credentials: "include",
+        headers: {
+          "Cache-Control": "max-age=0",
+          "Upgrade-Insecure-Requests": "1"
+        },
+        method: "POST"
+      }
+    );
 
-  if (response.status === 200) {
-    window.location.reload();
-    return;
-  } else {
+    if (response.status === 200) {
+      window.location.replace(redirectUrl);
+      return;
+    } else {
       throw new Error(`[bss] Failed to login: ${response.statusText}`);
     }
-    
   } catch (error) {
     handleError("utils/handleLogin", error);
   }
