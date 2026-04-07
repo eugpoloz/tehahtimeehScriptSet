5dee44093a278794f16e2f62068ecd258d704602
Evgenia Polozova <eugpoloz@gmail.com>
2026-04-08 01:03:45 +0300

    feat(optional): add new module!

---
+++ src/optional.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/*
+  tehahtimeehScriptSet/tehOptional
+  author: eugpoloz (грандоченька смерти)
+  license: MIT
+
+  вопросы и поддержка:
+  — https://github.com/eugpoloz/tehahtimeehScriptSet
+  — http://urchoice.rolka.su/profile.php?id=4789
+*/
+
+import randomPortraitGenerator from "./features/optional/randomPortraitGenerator";
+
+// module exports
+export { randomPortraitGenerator };
+
+// config example
+//
+//tehOptional.randomPortraitGenerator({
+//  howMany: 4,
+//  group_id: [1, 2, 5],
+//  filteredUsers: ["Hedge Bitch"],
+//  placeholder: "https://forumstatic.ru/files/001c/ab/7e/68132.png?v=1",
+//  selector: ".hehe-portrait",
+//});
