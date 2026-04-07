d0208f2dd9969e038f41018e6da3630eabe2182a
Evgenia Polozova <eugpoloz@gmail.com>
2026-04-08 01:02:49 +0300

    feat(tehFooter): enhance transformProfiles w/ handlers for some other scenarios

--- src/features/default/transformProfiles.js
+++ src/features/default/transformProfiles.js
@@ -1,28 +1,96 @@
 import DOMPurify from "dompurify";
+import { hasTopic } from "../../utils";
 
-const transformProfiles = () => {
-  const replaceFldContentWithHTML = (parent, selector) => {
-    const fldsToReplace = parent.querySelectorAll(selector);
+const handleOnlineIndicators = (post) => {
+  const isOnline = post
+    .querySelector(".post-author")
+    .classList.contains("online");
 
-    fldsToReplace.forEach((fld) => {
-      if (fld.textContent.includes("<")) {
-        const cleanContent = DOMPurify.sanitize(fld.textContent, {
-          ADD_ATTR: ["target"],
-          ADD_TAGS: ["strong"]
-        });
+  const paOnline = post.querySelector(".pa-online");
+  const paLastVisit = post.querySelector(".pa-last-visit");
 
-        fld.innerHTML = cleanContent;
+  if (paOnline || paLastVisit) {
+    const title = `"${
+      isOnline
+        ? `Онлайн ${paOnline?.textContent.split("Активен")[1]?.trim() ?? "∞"}`
+        : (paLastVisit?.textContent.split("визит:").join("визит: ") ??
+          "Трогает траву")
+    }"`;
+    const status = isOnline ? "on" : "off";
+
+    const html = `<li class="pa-online" data-ready="1" data-online=${status} title=${title}></li>`;
+
+    paOnline?.remove();
+    paLastVisit?.remove();
+
+    post.querySelector(".pa-posts").insertAdjacentHTML("beforebegin", html);
+  }
+};
+
+const addTitleToIconFields = (post, field) => {
+  const CONTENT_CLASS_NAME = "fld-content";
+
+  let fieldNode = post.querySelector(field);
+
+  if (!!fieldNode) {
+    const fieldName = fieldNode.querySelector(".fld-name")?.textContent;
+
+    if (field === ".pa-respect") {
+      const contentSpan = fieldNode.querySelector("span:not(.fld-name)");
+      contentSpan.title = fieldName;
+      contentSpan.classList.add(CONTENT_CLASS_NAME);
+      return;
+    }
+
+    const childNodes = fieldNode.childNodes;
+    for (const node of childNodes) {
+      if (node.nodeType === Node.TEXT_NODE) {
+        const html = `<span class="${CONTENT_CLASS_NAME}" title="${fieldName}">${node.textContent.trim()}</span>`;
+
+        const range = document.createRange();
+        const fragment = range.createContextualFragment(html);
+        node.replaceWith(fragment);
       }
-      fld.dataset.ready = "1";
+    }
+  }
+};
+
+const replaceFldContentWithHTML = (parent, selector) => {
+  const fldsToReplace = parent.querySelectorAll(selector);
+
+  fldsToReplace.forEach((fld) => {
+    if (fld.textContent.includes("<")) {
+      const cleanContent = DOMPurify.sanitize(fld.textContent, {
+        ADD_ATTR: ["target"],
+        ADD_TAGS: ["strong"]
+      });
+
+      fld.innerHTML = cleanContent;
+    }
+    fld.dataset.ready = "1";
+  });
+};
+
+const transformProfiles = (config = {}) => {
+  const fieldsWithTitle = config?.fieldsWithTitle ?? [
+    ".pa-posts",
+    ".pa-fld4",
+    ".pa-respect"
+  ];
+
+  if (hasTopic) {
+    const posts = document.querySelectorAll(".post");
+
+    posts.forEach((post) => {
+      replaceFldContentWithHTML(post, 'li[class^="pa-fld"]');
+      handleOnlineIndicators(post);
+
+      if (Array.isArray(fieldsWithTitle)) {
+        fieldsWithTitle?.forEach((field) => {
+          addTitleToIconFields(post, field);
+        });
+      }
     });
-  };
-
-  if (typeof FORUM.topic === "object") {
-    const profiles = document.querySelectorAll(".post-author ul");
-
-    profiles.forEach((profile) =>
-      replaceFldContentWithHTML(profile, 'li[class^="pa-fld"]')
-    );
   }
 
   if (!!document.getElementById("viewprofile-next")) {
