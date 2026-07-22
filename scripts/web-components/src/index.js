9de020ca7670035f7af2c3c6425ea791f59af1e2
Evgenia Polozova <eugpoloz@gmail.com>
2026-07-22 22:42:54 +0300

    feat(web-components): jsdoc & type check

--- scripts/web-components/src/index.js
+++ scripts/web-components/src/index.js
@@ -1,5 +1,10 @@
 "use strict";
 
+/**
+ * `@teh/web-components` entry: registers `theme-subject` and exports
+ * `defineAgeFromBirthday` for optional custom-tag registration.
+ */
+
 import ThemeSubject from "./features/webComponents/theme-subject";
 import defineAgeFromBirthday from "./features/webComponents/age-from-birthday";
 
