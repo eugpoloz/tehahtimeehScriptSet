e719375aa2c1a0f8d95eac1c5a9c90a22c437a01
Evgenia Polozova <eugpoloz@gmail.com>
2026-08-11 23:28:47 +0300

    refactor(hehedges-specials): rename from web-components

--- scripts/hehedges-specials/vite.config.js
+++ scripts/hehedges-specials/vite.config.js
@@ -2,7 +2,6 @@ import { createIifeConfig } from "../../tooling/vite-iife.config.js";
 
 export default createIifeConfig({
   entry: "./src/index.js",
-  fileName: "web-components",
-  name: "teh.webComponents",
-  globals: { webComponents: "teh" }
+  fileName: "hehedges-specials",
+  name: "teh"
 });
