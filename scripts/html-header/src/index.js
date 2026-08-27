b00325341677f5742b2ec3f62d6adc3c7fd5dfb2
Evgenia Polozova <eugpoloz@gmail.com>
2026-08-28 02:24:07 +0300

    refactor(html-header,html-footer): move change-font-size to header, inline style controls

--- scripts/html-header/src/index.js
+++ scripts/html-header/src/index.js
@@ -2,8 +2,8 @@
 
 import configureCoreSettings from "./features/configure-core-settings";
 import configureEditor from "./features/configure-editor";
-import addStyleControls from "./features/add-style-controls";
+import changeFontSize from "./features/change-font-size";
 
 configureCoreSettings();
 
-export { addStyleControls, configureEditor };
+export { changeFontSize, configureEditor };
