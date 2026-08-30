4065699a8ffe8a4fefcf1ad24d73381976386287
Evgenia Polozova <eugpoloz@gmail.com>
2026-08-30 15:19:23 +0300

    chore: run yarn format

--- scripts/character-vault/src/constants.js
+++ scripts/character-vault/src/constants.js
@@ -1,3 +1,4 @@
 export const ALL_FILTER = "__all__";
 export const CHAR_PARAM = "char";
-export const CHARACTERS_SCRIPT_URL = "//forumstatic.ru/files/001c/ab/7e/10010.js";
+export const CHARACTERS_SCRIPT_URL =
+  "//forumstatic.ru/files/001c/ab/7e/10010.js";
